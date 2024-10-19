b969824c2107b4936b3052146cba0bd9dc425ed8
Selinabernike <selinabernike@gmail.com>
2024-10-19 19:55:10 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+let index = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide() {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+    index = (index + 1) % slides.length;
+}
+
+setInterval(showSlide, 3000); // Ganti slide setiap 3 detik
+showSlide(); // Tampilkan slide pertama
